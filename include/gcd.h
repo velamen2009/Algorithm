4d05f00de6a18127338b25195402245bcb5a2744
velamen2009 <2565570621@qq.com>
2016-04-28 10:03:35 -0700

    add gcm

--- include/gcd.h
+++ include/gcd.h
@@ -12,7 +12,7 @@ namespace alg {
 	 * get the greatest common divisor for two elements
 	 */
 	template<typename T>
-	static T gcd(T u, T v){
+	static T gcd_minus(T u, T v){
 		T t(u);
 		while(u > 0){
 			if(u < v){
@@ -23,6 +23,55 @@ namespace alg {
 			u = u - v;
 		}
 		return v;
+	}
+	
+	/*
+	 * get the greatest common divisor for two elements
+	 */
+	template<typename T>
+	static T gcd_division(T u, T v){
+		T c;
+		while(v!=0){
+			c=u%v;
+			u=v;
+			v=c;
+		}
+		return u;
+	}
+	
+	/*
+	 * get the least common multiple for two elements
+	 */
+	template<typename T>
+	static T gcm_minus(T u, T v){
+		T a(u);
+		T b(v);
+		T t(u);
+		while(u > 0){
+			if(u < v){
+				t = u;
+				u = v;
+				v = t;
+			}
+			u = u - v;
+		}
+		return a*b/v;
+	}
+	
+	/*
+	 * get the least common multiple for two elements
+	 */
+	template<typename T>
+	static T gcm_division(T u, T v){
+		T a(u);
+		T b(v);
+		T c;
+		while(v!=0){
+			c=u%v;
+			u=v;
+			v=c;
+		}
+		return a*b/u;
 	}
 }
 
--- src/gcd_demo.cpp
+++ src/gcd_demo.cpp
@@ -15,14 +15,13 @@ int main()
 	srand(time(NULL));
 	// generate random numbers and fill them to the list
 	for(int i = 0; i < MAX_ELEMENTS; i++ ){
-		list[i] = rand()%100;
+		list[i] = rand()%100+1;
 	}
-	printf("The list before sorting is:\n");
+	printf("The list is:\n");
 	printlist(list,MAX_ELEMENTS);
-
 	// get gcd for each pair of data
 	for(int i=0; i!=MAX_ELEMENTS/2; ++i){
-		std::cout<<list[2*i]<<"\t"<<list[2*i+1]<<"\t"<<gcd(list[2*i],list[2*i+1])<<"\n";
+		std::cout<<list[2*i]<<"\t"<<list[2*i+1]<<"\t"<<gcd_minus(list[2*i],list[2*i+1])<<"\t"<<gcd_division(list[2*i],list[2*i+1])<<"\t"<<gcm_minus(list[2*i],list[2*i+1])<<"\t"<<gcm_division(list[2*i],list[2*i+1])<<"\n";
 	}
 	std::cout<<std::endl;
 	return 0;
