fb0afac23603d3f0696dc97efd5031b377ae5259
velamen2009 <2565570621@qq.com>
2016-04-29 14:02:01 -0700

    added boule linked list

---
+++ include/double_linked_list.h
@@ -0,0 +1,144 @@
+/*******************************************************************************
+ * DOUBLE LINKED-LIST FROM LINUX KERNEL
+ *
+ * http://en.wikipedia.org/wiki/Double_linked_list
+ ******************************************************************************/
+
+#ifndef __DOUBLE_LINKED_LIST_H__
+#define __DOUBLE_LINKED_LIST_H__
+
+#include <iostream>
+
+using namespace std;
+
+namespace alg{
+	template<typename T>
+	struct DoublyLinkedNode{
+		DoublyLinkedNode<T>* prev;
+		DoublyLinkedNode<T>* next;
+		T data;
+		DoublyLinkedNode(T t){
+			prev = 0;
+			next = 0;
+			data = t;
+		}
+	};
+	
+	template<typename T>
+	class DoublyLinkedList{
+	public:
+		DoublyLinkedList():head(0),tail(0){}
+		void insertAfter(T u, T v){
+			DoublyLinkedNode<T>* node = searchNode(u);
+			if(NULL==node){
+				return;
+			}
+			return insertAfter(node, new DoublyLinkedNode<T>(v));
+		}
+		void insertBefore(T u, T v){
+			DoublyLinkedNode<T>* node = searchNode(u);
+			if(NULL==node){
+				return;
+			}
+			insertBefore(node, new DoublyLinkedNode<T>(v));
+		}
+		void insertHead(T t){
+			insertHead(new DoublyLinkedNode<T>(t));
+		}
+		void insertTail(T t){
+			insertTail(new DoublyLinkedNode<T>(t));
+		}
+		void remove(T t){
+			DoublyLinkedNode<T>* node = searchNode(t);
+			remove(node);
+		}
+		void print(){
+			DoublyLinkedNode<T>* p = head;
+			while(NULL!=p){
+				cout<<p->data<<"\t";
+				p=p->next;
+			}
+			cout<<endl;
+		}
+		void print_reverse(){
+			DoublyLinkedNode<T>* p = tail;
+			while(NULL!=p){
+				cout<<p->data<<"\t";
+				p=p->prev;
+			}
+			cout<<endl;
+		}
+	private:
+		DoublyLinkedNode<T>* searchNode(T t){
+			DoublyLinkedNode<T>* p = head;
+			while(NULL!=p){
+				if(t==p->data){
+					return p;
+				}
+				p=p->next;
+			}
+			return 0;
+		}
+		void insertAfter(DoublyLinkedNode<T>* node, DoublyLinkedNode<T>* newNode){
+			newNode->prev = node;
+			newNode->next = node->next;
+			if(0==node->next){
+				tail = newNode;
+			}
+			else{
+				node->next->prev = newNode;
+			}
+			node->next = newNode;
+		}
+		void insertBefore(DoublyLinkedNode<T>* node, DoublyLinkedNode<T>* newNode){
+			newNode->prev = node->prev;
+			newNode->next = node;
+			if(0==node->prev){
+				head = newNode;
+			}
+			else{
+				node->prev->next = newNode;
+			}
+			node->prev = newNode;
+		}
+		void insertHead(DoublyLinkedNode<T>* newNode){
+			if(0==head){
+				head = newNode;
+				tail = newNode;
+			}
+			else{
+				insertBefore(head, newNode);
+			}
+		}
+		void insertTail(DoublyLinkedNode<T>* newNode){
+			if(0==tail){
+				head = newNode;
+				tail = newNode;
+			}
+			else{
+				insertAfter(tail, newNode);
+			}
+		}
+		void remove(DoublyLinkedNode<T>* node){
+			if(NULL==node){return;}
+			if(0==node->prev){
+				head = node->next;
+			}
+			else{
+				node->prev->next = node->next;
+			}
+			if(0==node->next){
+				tail = node->prev;
+			}
+			else{
+				node->next->prev = node->prev;
+			}
+			delete node;
+		}
+	private:
+		DoublyLinkedNode<T>* head;
+		DoublyLinkedNode<T>* tail;
+	};
+}
+#endif
+
---
+++ src/double_linked_list_demo.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <time.h>
+
+#include "../include/generic.h"
+#include "../include/double_linked_list.h"
+
+int main()
+{
+	RANDOM_INIT();
+	using namespace alg;
+	using namespace std;
+
+	srand(time(NULL));
+	
+	DoublyLinkedList<char> charDList;
+	
+	for(int i = 0; i != 10; ++i ){
+		charDList.insertTail(rand()%26+65);
+	}
+	cout<<"Initial 10 times insert at tail:"<<endl;
+	charDList.print();
+	
+	cout<<"3 times insert at head:"<<endl;
+	char c = rand()%26+65;
+	charDList.insertHead(c);
+	charDList.insertHead(c);
+	charDList.insertHead(c);
+	charDList.print();
+	
+	cout<<"Insert before and after:"<<endl;
+	char d = (c+1-65)%26+65;
+	char e = (d+1-65)%26+65;
+	charDList.insertAfter(c, d);
+	charDList.insertBefore(d, e);
+	charDList.print();
+	
+	cout<<"Remove:"<<endl;
+	charDList.remove(e);
+	charDList.remove(d);
+	charDList.print();
+	
+	cout<<"Reverse order:"<<endl;
+	charDList.print_reverse();
+	
+	return 0;
+} //
